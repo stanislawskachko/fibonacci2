7469c382e183e4cdb69fc73e037dfad6153811b1
Stanislav Skachko <stanislawskachko@gmail.com>
2018-09-05 13:17:33 +0000

    3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 //Код для обчислення в циклі
 function getData(){
     let n=+ document.getElementById("fibcyc").value;
+    if (isNaN(n)) {document.getElementById("answercyc").innerHTML = 'Помилка - введіть число';}
+    else {
 function fib(n) {
     let answer;
     let a = 1, b = 1;
@@ -14,18 +16,19 @@ function fib(n) {
 }
 document.getElementById("answercyc").innerHTML = 'Число ' + n + ' ряду Фібоначчі рівне ' + fib(n);
 }
-
-
+}
 
 //Код для обчислення рекурсивно
 function getDataRec() {
     let m=+ document.getElementById("fibrecurs").value;
+    if (isNaN(m)) {document.getElementById("answerrec").innerHTML = 'Помилка - введіть число';}
+    else {
 function fibrec(m) { 
    if (m <= 2){return 1;}
    else{return fibrec(m-2) + fibrec(m-1);}
 }
 document.getElementById("answerrec").innerHTML = 'Число ' + m + ' ряду Фібоначчі рівне ' + fibrec(m);
 }
-
+}
 
 
